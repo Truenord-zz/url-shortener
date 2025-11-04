270b3105f759fa51c14d36d930c5f40a5c7f1b24
Andreas Carlson <andreascarlson19@gmail.com>
2025-11-04 21:57:30 +0100

    inital database.py version

--- database.py
+++ database.py
@@ -1,15 +1,32 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
 # database address
 
 # get and set methods
 
 # create the engine and give it an address
-DATABASE_URL = "sqlite:///./shortener.db"
-engine = create_engine(...)
+DB_ADDRESS = "sqlite:///./shortener.db"
+url_engine = create_engine(
+    DB_ADDRESS,
+    connect_args={"check_same_thread": False}
+) # makes it so fastapi can use sqlite, todo concurrency? maybe full scale server-client setup
 
-# this will get the seesion running
-SessionLocal = sessionmaker(...)
+# this will get the session running
+SessionLocal = sessionmaker(
+    autoflush=False, # initally lets control when we update db
+    bind=url_engine
+)
 
-# this builds the tabels
-Base.metadata.create_all(bind=engine)
+# this builds the tabels, for each object inheriting from base
+Base.metadata.create_all(bind=url_engine)
 
-# should open con, do something, close it
+# very barebones, create session, give db con to some caller and then close it
+def get_db():
+    db = SessionLocal() # init a session
+    
+    try:
+        yield db #lazy yield
+    finally: #we gotta close it, always!
+        db.close() 
+        
