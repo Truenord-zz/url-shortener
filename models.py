3fe6ed32ead1f70f2f857fc824dbc11e3576ba4f
Andreas Carlson <andreascarlson19@gmail.com>
2025-11-04 21:45:17 +0100

    models.py now creates a table with urls each with some properties

--- models.py
+++ models.py
@@ -4,8 +4,13 @@ from datetime import datetime
 
 Base = declarative_base()
 
-class URL(Base):
-    pass
-
 # We want the database to have these fields
 # id, original_url, new_url, creation_time, views.
+class URL(Base):
+    __tablename__ = "urls" 
+    # coloumns for each row: url
+    id = Column(Integer, primary_key=True, index=True, nullable=False)
+    original_url = Column(String, nullable=False)
+    short_url = Column(String, unique=True, index=True, nullable=False)
+    creation_time = Column(DateTime, default=datetime.now(datetime.timezone.utc), nullable=False)
+    views = Column(Integer, default=0, nullable=False)
