71a5716e272c1aaa534690f94ef31fcc5f25051d
Andreas Carlson <andreascarlson19@gmail.com>
2025-11-04 22:12:23 +0100

    function that generates random adresses

--- utils.py
+++ utils.py
@@ -1,2 +1,11 @@
+import random
+import string
 # utility functions, one file for now.
 # probabily we should generate the new urls here
+
+def short_url_generator(new_url_length = 6):
+    chars = string.digits + string.ascii_letters
+    new_url = ''.join(random.choice(chars) for i in range(new_url_length))
+    return new_url
+
+print(short_url_generator())
